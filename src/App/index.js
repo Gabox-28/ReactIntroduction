0bfbd59dd21b94dd8a0f1c0f0cfb1bb41263d4f7
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-09-28 05:04:28 -0600

    Cambios para el deploy

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,7 @@
 import {AppUI} from "./AppUI";
-import {useLocalStorage} from "../TodoContext/useLocalStorage";
 import './App.css'
 import React from "react";
 import {TodoProvider} from "../TodoContext";
-
-const defaultTodos = [
-  {text: 'Cortar cebolla', completed: false},
-  {text: 'Terminar el curso de react', completed: false},
-  {text: 'Llorar con la Llorona', completed: true},
-]
 
 function App() {
   return (
